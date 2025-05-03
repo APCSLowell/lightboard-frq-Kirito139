4bea6e5323eb15aef45abff6f40ab5e524338ff3
Lennier Mulder <lenniermulder@icloud.com>
2025-05-02 22:19:36 -0700

    First attempt

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -1,47 +1,61 @@
-public class LightBoard
-{
-  /** The lights on the board, where true represents on and false represents off.
-   */
-  private boolean[][] lights;
+public class LightBoard {
+    /** The lights on the board, where true represents on and false represents
+     * off.
+     */
+    private boolean[][] lights;
 
-  /** Constructs a LightBoard object having numRows rows and numCols columns.
-   * Precondition: numRows > 0, numCols > 0
-   * Postcondition: each light has a 40% probability of being set to on.
-   */
-  public LightBoard(int numRows, int numCols)
-  {
-    /* to be implemented in part (a) */
+    /** Constructs a LightBoard object having numRows rows and numCols columns.
+     * Precondition: numRows > 0, numCols > 0
+     * Postcondition: each light has a 40% probability of being set to on.
+     */
+    public LightBoard (int numRows, int numCols) {
+        /* to be implemented in part (a) */
+        lights = new boolean[numRows][numCols];
 
-  }
-
-  /** Evaluates a light in row index row and column index col and returns a status
-   *  as described in part (b).
-   *  Precondition: row and col are valid indexes in lights.
-   */
-  public boolean evaluateLight(int row, int col)
-  {
-    /* to be implemented in part (b) */
-   
- 
-  }
-  public boolean[][] getLights()
-  {
-    return lights;
-  }
-  //used for testing
-  public String toString()
-  {
-    String s = "";
-    for (int r = 0; r < lights.length; r++)
-    {
-      for (int c = 0; c < lights[0].length; c++)
-        if (lights[r][c])
-          s += "*";
-        else
-          s += ".";
-      s += "\n";
+        for (int r=0; r < numRows; r++) {
+            for (int c=0; c < numCols; c++) {
+                lights[r][c] = Math.random() < 0.4;
+            }
+        }
     }
-    return s;
-  }
-  
+
+    /** Evaluates a light in row index row and column index col and returns a status
+     *  as described in part (b).
+     *  Precondition: row and col are valid indexes in lights.
+     */
+
+    public boolean evaluateLight(int row, int col) {
+        /* to be implemented in part (b) */
+        int sum = 0;
+
+        for (boolean lights : lights[row]) { if (lights) sum++; }
+        
+        if (lights[row][col]) {
+            if (sum % 2 == 0) return false;
+            else return true;
+        } else {
+            if (sum % 3 == 0) return true;
+            else return false; 
+        }
+    }
+    public boolean[][] getLights()
+    {
+        return lights;
+    }
+    //used for testing
+    public String toString()
+    {
+        String s = "";
+        for (int r = 0; r < lights.length; r++)
+        {
+            for (int c = 0; c < lights[0].length; c++)
+                if (lights[r][c])
+                    s += "*";
+                else
+                    s += ".";
+            s += "\n";
+        }
+        return s;
+    }
+
 }
